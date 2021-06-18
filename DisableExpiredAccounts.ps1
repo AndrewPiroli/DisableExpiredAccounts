dffe1c8a33ed1b580dfe3eec3a77bcc610af6cc2
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2021-06-18 00:06:04 -0400

    Initial import

---
+++ DisableExpiredAccounts.ps1
@@ -0,0 +1,112 @@
+﻿# SPDX-License-Identifier: MIT
+
+# User servicable options
+$TestMode = $true
+
+$UseEventLog = $true
+$EventLogDest = "Application"
+$EventLogSource = "DisableExpiredAccounts"
+$SendEmailReport = $true
+$PSEmailServer = "smtp.dundermifflin.com"
+# No Touch!
+$EmailRecipients = @()
+# Ok, now you can touch again.
+# Add email recipients in this format
+$EmailRecipients += "Michael Scott <mscott@dundermifflin.com>"
+# End user sericable options
+
+# Setup Event Log
+if ($UseEventLog){
+    $Error.Clear()
+    New-EventLog -LogName $EventLogDest -Source $EventLogSource
+    # Powershell is dumb, and some exceptions can't be caught with a try-catch (why?)
+    if ($Error[0]){
+        if ($Error[0].ToString().Contains("Access is denied")){
+            Write-Warning "Could not create EventLog Source- Access Denied"
+            $UseEventLog = $false
+        }
+        if ($Error[0].ToString().Contains("source is already registered")){
+            Write-Warning "EventLog already registered"
+        }
+    }
+}
+
+if ($UseEventLog){
+    Write-EventLog -LogName $EventLogDest -Source $EventLogSource -EventId 100 -Message "Disable Expired Accounts Script: Startup Complete"
+}
+
+# Set up a report for later at the end.
+$report = @{}
+$report.Add("User Principle Name", "Status")
+$report.Add("@@@@@@@@@@@@@@@@@@@", "@@@@@@")
+$warnings = 0
+$errors = 0
+
+# Pull all expired user accounts
+$ExpiredAccounts = Search-ADAccount -AccountExpired -UsersOnly
+
+# Loop over all results
+foreach ($account in $ExpiredAccounts){
+    # If the account is already disabled, we don't care, so skip it and move on to the next one. Do not add to the final report.
+    if ($account.Enabled -eq $false){
+        Continue
+    }
+    # Make sure the account shows no activity since expiration, this would be really weird. Loudly complain if this happens.
+    if ($account.LastLogonDate -gt $account.AccountExpirationDate){
+        $report.Add($account.UserPrincipleName, "ERROR: LastLogonDate newer than AccountExpirationDate. Account left untouched!")
+        Write-Warning "Account Logged on since expiry. This should be impossible!!"
+        if ($UseEventLog){
+            Write-EventLog -LogName $EventLogDest -Source $EventLogSource -EventId 500 -EntryType "Error" -Message "Account $($account.UserPrincipleName) has a LastLogonDate newer than its Expiration Date! Refusing to work on insane accounts"
+        }
+        $warnings++
+        Continue
+    }
+    # Time to disable their account
+    try{
+        # Powershell is dumb, and does not let us try-catch-else, so we have to have to have this.....
+        $DisableSuccess = $false
+        Disable-ADAccount -Identity $account -WhatIf:$TestMode
+        $DisableSuccess = $true
+    }
+    catch{
+        Write-Warning "Disable-ADAccount failed - this usually means bad credentials!"
+        if ($UseEventLog){
+            Write-EventLog -LogName $EventLogDest -Source $EventLogSource -EventId 403 -EntryType "Error" -Message "Error disabling $($account.UserPrincipleName), suspect my user does not have Account Operator or better permissions"
+        }
+        $errors++
+    }
+    if ($DisableSuccess){
+        $report.Add($report.Add($account.UserPrincipleName, "SUCCESS: Account disabled!"))
+        Write-Warning "Disabled $($account.UserPrincipleName)"
+        if ($UseEventLog){
+            Write-EventLog -LogName $EventLogDest -Source $EventLogSource -EventId 200 -Message "Account $($account.UserPrincipleName) disabled"
+        }
+    }
+}
+
+# Build report
+$EmailSubject = "DisableExpiredAccounts - Errors: $errors Warnings: $warnings"
+$EmailBody = $report | Format-Table -HideTableHeaders
+
+if ($TestMode){
+    $EmailSubject += " TEST MODE"
+    $EmailBody = "TEST MODE ENABLED`r`n$EmailBody"
+}
+
+if ($SendEmailReport){
+    $LocalHostname = $env:COMPUTERNAME
+    $LocalDomain = Get-WMIObject Win32_ComputerSystem| Select-Object -ExpandProperty Domain
+    Send-MailMessage -From "$LocalHostname@$LocalDomain" -To $EmailRecipients -Subject $EmailSubject -Body $EmailBody
+    Write-Warning "attempted to send email report"
+    if ($UseEventLog){
+        Write-EventLog -LogName $EventLogDest -Source $EventLogSource -EventId 200 -Message "Attemped to send email report!"
+    }
+}
+
+Write-Warning $EmailSubject
+Write-Warning $EmailBody
+
+if ($UseEventLog){
+    Write-EventLog -LogName $EventLogDest -Source $EventLogSource -EventId 200 -Message "DisableExpiredAccounts script - done!"
+}
+Write-Warning "DisableExpiredAccounts script - done!"
---
+++ README.md
@@ -0,0 +1,24 @@
+# Disable Expired Accounts
+
+A PS script that will safely disable expired AD accounts.
+
+# Instructions
+
+Run the script as a user with permissions to disable accounts, maybe set it up as a scheduled task, if you want.
+
+## User options
+
+User serviceable options:
+$TestMode, set to $true or $false to enable a dry run. No Changes will be made in AD
+
+$UseEventLog, set to $true or $false to enable Event Log logging.
+
+$EventLogDest, set to a string (in quotes) to the Event Log that will be used for logging.
+
+$EventLogSource, set to a string (in quotes) to the name of the source the script will log as to the Event Log
+
+$SendEmailReport, set to $true or $false to enable the email report.
+
+$$PSEmailServer, set to a string (in quotes) to the smtp server that the email report will be sent via
+
+To set email recipients, add `$EmailRecipients += "Michael Scott <mscott@dundermifflin.com>"` to the end of the marked user serviceable options area. Obviously replace the name and email with your own, just use the same format (I can't belive I have to say that). Add as many as you like (probably?)
